b4729f8cae38a85760f20afdc62169abd9059fbf
Shaibee19 <shaipattzgray@gmail.com>
2025-06-03 00:11:58 -0400

    integrated the email into the .js

---
+++ index.js
@@ -0,0 +1,27 @@
+// template_j57chsp
+// service_lq4pqm1
+// M_OoP5ANfy9nBleaM
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_lq4pqm1",
+      "template_j57chsp",
+      event.target,
+      "M_OoP5ANfy9nBleaM"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at shaipattzgray@gmail.com"
+      );
+    });
+}
